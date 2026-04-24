6e9e8d6e6aa080f75c3482ec8fc5d4609568bcc6
andrey_norvatov <whitesgn4@gmial.com>
2026-04-24 15:58:52 +0400

    downgrade main_sync.py

--- check_uploaded_files/main_sync.py
+++ check_uploaded_files/main_sync.py
@@ -1,12 +1,13 @@
 import os
-# import pandas as pd
+import pandas as pd
 import requests
 
 from datetime import datetime
 
 HOST = "https://tops-stage.mos.ru"
 DP_HOST = "http://10.126.145.36:3000"
-Token = "eyJhbGciOiJFUzM4NCIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZTA1ZmNkZTEtMGUyMi00ZDI3LTgxM2ItYmM2NWJjY2E4N2MzIiwidG9rZW5faWQiOiI0NGE1YzgyMS0zYmYzLTQ1ZDEtYjg1YS04ZWZkNmY4NTQ2NjgiLCJyb2xlIjoiT1BFUkFUT1IiLCJleHAiOjE3NzcwMTgzOTl9.0IdXEi9UT2w1AQ9YEnKWgoJr_t2wx9JTW7nBkymgzCBS4Sgxor4wbpjX09JC09d-PpdTVgNIpEQAYB1zFCorgnEzB3Mr1NzbLUJMF9yfr1AAMfx8wiPQS4-bE5i57n_o"
+Token = "eyJhbGciOiJFUzM4NCIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTUzN2RlMTQtOTAyOC00ZjIxLThkNDQtOTRlMDVkMDRmNzBjIiwidG9rZW5faWQiOiI1MDI2MmNhMC1jNjgwLTRiNTYtYWE0Yi01ZGI1NjliMWYzYzIiLCJyb2xlIjoiT1BFUkFUT1IiLCJleHAiOjE3NzY5NDc0MTV9.Er6hqRAAbdYNJyHw9ZBNiMyVMR9cUazRAgAWS1ZtU23OSYiOWQXx33ULp8UrKFDc-DZB99YN10SqTM1rRZSNbWTMdfF8EKtphr3SICsMu3jeggb-yTl_pNlmInAGGu6m"
+
 
 class Row_BD:
     def __init__(self, data_dict):
@@ -23,24 +24,23 @@ class Row_BD:
     def get_task(self, Token):
         headers = {'x-lamb-auth-token': Token}
         response = requests.get(f'{HOST}/api/mona/background_tasks/{self.task_id}', headers=headers)
-        print(response.request.url)
-        print(response.request.headers)
-        print(response.json())
         return response.json()
-    
+
     def get_raw(self, Token):
         headers = {'x-lamb-auth-token': Token}
-        response = requests.get(f'{HOST}/api/chiara/projection_data/{self.projection_id}/{self.row_id}', headers=headers)
+        response = requests.get(f'{HOST}/api/chiara/projection_data/{self.projection_id}/{self.row_id}',
+                                headers=headers)
         return response.json()
-    
+
     def get_raw_filter(self, Token):
         headers = {'x-lamb-auth-token': Token}
         params = {
-        "filters": f"[[{{\"column\":\"mobil_nyj_telefon_1\",\"to_lower\":true,\"operator\":\"EQUAL\",\"value\":\"{self.mob}\"}}]]"
+            "filters": f"[[{{\"column\":\"mobil_nyj_telefon_1\",\"to_lower\":true,\"operator\":\"EQUAL\",\"value\":\"{self.mob}\"}}]]"
         }
-        response = requests.get(f'{HOST}/api/chiara/projection_data/{self.projection_id}/', headers=headers, params=params)
+        response = requests.get(f'{HOST}/api/chiara/projection_data/{self.projection_id}/', headers=headers,
+                                params=params)
         return response.json()
-    
+
     def change_status(self):
         params = {'p_id': self.id}
         response = requests.post(f'{DP_HOST}/rpc/tops_checkupdate_set_read_line', json=params)
@@ -60,7 +60,7 @@ class Row_BD:
             'email': email
         }
         return result
-    
+
     def check_object(self, object):
         result_mob = self.extract_digits(object.get('mob'))
         result_email = object.get('email')
@@ -76,7 +76,6 @@ class Row_BD:
             print(f'Проверка task_id = {self.task_id} прошла успешно')
             return f'OK'
 
-
     def check_task(self, task):
 
         status = task.get("status")
@@ -88,7 +87,7 @@ class Row_BD:
 
     def extract_digits(self, phone_str):
         return ''.join(ch for ch in phone_str if ch.isdigit())
-    
+
     def time_exec(self, task):
         time_created = datetime.fromisoformat(task.get("time_created").replace('Z', '+00:00'))
         time_updated = datetime.fromisoformat(task.get("time_updated").replace('Z', '+00:00'))
@@ -96,6 +95,7 @@ class Row_BD:
         result_second = delta.total_seconds()
         result = f'{result_second:.3f} секунд'
         return result
+
 
 def save_result_to_csv(data):
     result = {
@@ -112,6 +112,7 @@ def save_result_to_csv(data):
     file_exists = os.path.isfile(csv_file)
     df.to_csv(csv_file, sep=';', mode='a', header=not file_exists, index=False, encoding='utf-8')
 
+
 def main():
     response = requests.post(f'{DP_HOST}/rpc/tops_checkupdate_get_all')
     result = response.json()
@@ -125,19 +126,19 @@ def main():
             task = row_obj.get_task(Token=Token)
             status = row_obj.check_task(task=task)
             status_row = row_obj.check_object(row_obj.get_object(Token=Token))
-    #
-    #         data['status_row'] = status_row
-    #         data['task_id'] = row_obj.task_id
-    #         data['status'] = status
-    #         data['start_time'] = row_obj.start_time
-    #         data['meta_info'] = task.get('meta_info')
-    #         data['file_name'] = row_obj.file_name
-    #         data['row_id'] = row_obj.row_id
-    #
-    #         row_obj.change_status()
-    #         if data:
-    #             save_result_to_csv(data=data)
-    # print(f'Время выполненения- {datetime.now() - start}')
+
+            data['status_row'] = status_row
+            data['task_id'] = row_obj.task_id
+            data['status'] = status
+            data['start_time'] = row_obj.start_time
+            data['meta_info'] = task.get('meta_info')
+            data['file_name'] = row_obj.file_name
+            data['row_id'] = row_obj.row_id
+
+            row_obj.change_status()
+            if data:
+                save_result_to_csv(data=data)
+    print(f'Время выполненения- {datetime.now() - start}')
 
 
 if __name__ == "__main__":
